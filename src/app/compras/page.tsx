923bf32fa614067885694b32e00827594476edc6
lcsilva76 <lsilva@fiap.com.br>
2024-10-29 08:33:23 -0300

    primeiro commit

---
+++ src/app/compras/page.tsx
@@ -0,0 +1,7 @@
+export default function Compras(){
+    return(
+        <main className="flex justify-center p-10">
+            <h1 className="text-center text-4xl font-bold text-indigo-800">Página de Compras</h1>
+        </main>
+    )
+}
